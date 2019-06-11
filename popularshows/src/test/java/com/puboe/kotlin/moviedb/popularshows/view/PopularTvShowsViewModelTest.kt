12151dd0c18b8ae5fff67492403e858d3cdd1b82
Pablo Diaz <pablo.diazuboe@mercadolibre.com>
2019-06-11 03:08:36 -0300

    Add view model tests

--- popularshows/src/test/java/com/puboe/kotlin/moviedb/popularshows/view/PopularTvShowsViewModelTest.kt
+++ popularshows/src/test/java/com/puboe/kotlin/moviedb/popularshows/view/PopularTvShowsViewModelTest.kt
@@ -15,8 +15,7 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mock
-import org.mockito.Mockito.`when`
-import org.mockito.Mockito.verify
+import org.mockito.Mockito.*
 import org.mockito.MockitoAnnotations
 
 class PopularTvShowsViewModelTest {
@@ -47,7 +46,7 @@ class PopularTvShowsViewModelTest {
 
     @Test
     @ExperimentalCoroutinesApi
-    fun getPopularTvShows_shouldShowResults() = runBlockingTest {
+    fun getPopularTvShows_withSuccess_shouldShowResults() = runBlockingTest {
         val show1 = TvShow("name1", 10F, "overview1", "/poster1")
         val show2 = TvShow("name2", 9F, "overview2", "/poster2")
         `when`(repository.getPopularTvShows()).thenReturn(DataResult.Success(listOf(show1, show2)))
@@ -59,5 +58,72 @@ class PopularTvShowsViewModelTest {
         assertThat(LiveDataTestUtil.getValue(viewModel.error)).isNull()
         assertThat(LiveDataTestUtil.getValue(viewModel.loading)).isFalse()
         assertThat(LiveDataTestUtil.getValue(viewModel.shows)).containsExactly(show1, show2)
+    }
+
+    @Test
+    @ExperimentalCoroutinesApi
+    fun getPopularTvShows_withError_shouldShowError() = runBlockingTest {
+        `when`(repository.getPopularTvShows()).thenReturn(DataResult.Error.NetworkError)
+
+        viewModel.getPopularTvShows()
+        testContext.triggerActions()
+
+        verify(repository).getPopularTvShows()
+        assertThat(LiveDataTestUtil.getValue(viewModel.error)).isEqualTo(DataResult.Error.NetworkError)
+        assertThat(LiveDataTestUtil.getValue(viewModel.loading)).isFalse()
+    }
+
+    @Test
+    @ExperimentalCoroutinesApi
+    fun requestNextPage_withSuccess_shouldShowResults() = runBlockingTest {
+        val show1 = TvShow("name1", 10F, "overview1", "/poster1")
+        val show2 = TvShow("name2", 9F, "overview2", "/poster2")
+        `when`(repository.getNextPage()).thenReturn(DataResult.Success(listOf(show1, show2)))
+
+        viewModel.requestNextPage()
+        testContext.triggerActions()
+
+        verify(repository).getNextPage()
+        assertThat(LiveDataTestUtil.getValue(viewModel.error)).isNull()
+        assertThat(LiveDataTestUtil.getValue(viewModel.loading)).isFalse()
+        assertThat(LiveDataTestUtil.getValue(viewModel.shows)).containsExactly(show1, show2)
+    }
+
+    @Test
+    @ExperimentalCoroutinesApi
+    fun requestNextPage_withError_shouldShowError() = runBlockingTest {
+        `when`(repository.getNextPage()).thenReturn(DataResult.Error.ServerError)
+
+        viewModel.requestNextPage()
+        testContext.triggerActions()
+
+        verify(repository).getNextPage()
+        assertThat(LiveDataTestUtil.getValue(viewModel.error)).isEqualTo(DataResult.Error.ServerError)
+        assertThat(LiveDataTestUtil.getValue(viewModel.loading)).isFalse()
+    }
+
+    @Test
+    @ExperimentalCoroutinesApi
+    fun listScrolled_overThreshold_shouldRequestPage() = runBlockingTest {
+        val show1 = TvShow("name1", 10F, "overview1", "/poster1")
+        val show2 = TvShow("name2", 9F, "overview2", "/poster2")
+        `when`(repository.getNextPage()).thenReturn(DataResult.Success(listOf(show1, show2)))
+
+        viewModel.listScrolled(5, 15, 20)
+        testContext.triggerActions()
+
+        verify(repository).getNextPage()
+        assertThat(LiveDataTestUtil.getValue(viewModel.error)).isNull()
+        assertThat(LiveDataTestUtil.getValue(viewModel.loading)).isFalse()
+        assertThat(LiveDataTestUtil.getValue(viewModel.shows)).containsExactly(show1, show2)
+    }
+
+    @Test
+    @ExperimentalCoroutinesApi
+    fun listScrolled_underThreshold_shouldNotRequestPage() = runBlockingTest {
+        viewModel.listScrolled(1, 1, 20)
+        testContext.triggerActions()
+
+        verify(repository, never()).getNextPage()
     }
 }
